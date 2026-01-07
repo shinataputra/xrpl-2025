98f806b6c3fdc48ee318d15cdbbf2a0024969186
shinataputra <shinata.putra@gmail.com>
2026-01-07 09:48:02 +0700

    Ubah struktur folder

---
+++ XRPL1/Semester Ganjil/Demo/inputan.py
@@ -0,0 +1,9 @@
+perempuan = input("Apakah perempuan itu selau benar? (ya/tidak): ")
+bawaan = (perempuan.lower() == "ya")
+print(f"Status bawaan perempuan: {bawaan}")
+
+
+
+
+# luaspersegi = int(panjang) * int(lebar)
+# print(f"Dengan panjang {panjang} dan lebar {lebar}, maka luas persegi adalah {luaspersegi}")
