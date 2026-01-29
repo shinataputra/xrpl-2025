b8c272366110238d425fa3841cdb0d8bf933a9be
shinataputra <shinata.putra@gmail.com>
2026-01-29 12:59:18 +0700

    add : logika login menggunakan while dan if

---
+++ XRPL1/Semester Genap/Terstruktur/Demo/1_while_menu.py
@@ -0,0 +1,12 @@
+while True:
+    print("Silahkan Login")
+    username = input("Masukan username : ")
+    password = input("Masukan password : ")
+
+    if username == "admin" and password == "admin123":
+        print("Login Berhasil")
+        break
+    else:
+        print("Username atau Password Salah, Silahkan Coba Lagi")
+
+    
